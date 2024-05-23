9efbea965f6503ea40cfbeddef61d322225324d5
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-05-23 19:17:15 -0300

    test

--- backend/obj/Debug/net8.0/backend-api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312e7d780df01654f49c7e719df47e37a7ead75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14e3f0727e304fecf444d757b2632fdf523a065")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
