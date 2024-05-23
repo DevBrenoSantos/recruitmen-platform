b9402b7fc20836fa4d7289aacf0971e2619ce535
Breno Amaral <contato.brenoamaral.s@gmail.com>
2024-05-23 19:13:27 -0300

    Atualização teste

--- backend/obj/Debug/net8.0/backend-api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b930aaecdc5868adc0fef29003dfcbddcf2521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1ddf6ac9afbf96252bfacf5def4f1cb3286403")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
